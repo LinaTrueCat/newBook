6cb566b54e8748161f0845347675b55212e73d30
LinaTrueCat <146427251+LinaTrueCat@users.noreply.github.com>
2025-04-06 19:05:48 +0300

    add mainDish.h and mainDish.cpp

--- receptCode/receptCode/nps.cpp
+++ receptCode/receptCode/nps.cpp
@@ -9,6 +9,7 @@ Nps::Nps(string n, int w, bool v) {
     showVegStatus();
     cout << endl;
 }
+
 Nps::Nps(string n, bool v) {
     this->name = n;
     this->wish = 0;
@@ -17,36 +18,22 @@ Nps::Nps(string n, bool v) {
 
 void Nps::showWish() const {
     switch (wish) {
-    case 1:
-        cout << "grecha" << endl;
-        break;
-    case 2:
-        cout << "potato" << endl;
-        break;
-    case 3:
-        cout << "surnuku" << endl;
-        break;
-    default:
-        cout << "nothing specific" << endl;
+    case 1: cout << "grecha" << endl; break;
+    case 2: cout << "potato" << endl; break;
+    case 3: cout << "surnuku" << endl; break;
+    default: cout << "nothing specific" << endl;
     }
 }
 
 void Nps::showVegStatus() const {
-    if (vegStatus) {
-        cout << "is vegetarian" << endl;
-    }
-    else {
-        cout << "is not vegetarian" << endl;
-    }
+    if (vegStatus) cout << "is vegetarian" << endl;
+    else cout << "is not vegetarian" << endl;
 }
 
 void Nps::chooseRecepy(const Recepy& recepy) const {
     cout << name << " decides to cook: " << endl;
     cout << "Recipe: " << recepy.getName() << ", Page: " << recepy.getPage() << endl;
-
-
     recepy.meatStatus();
-
 }
 
 Nps::~Nps() {}
