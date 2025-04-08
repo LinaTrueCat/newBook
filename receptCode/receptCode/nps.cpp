af57c905a513937f94d9481da4ff8a1d8da06e20
LinaTrueCat <146427251+LinaTrueCat@users.noreply.github.com>
2025-04-09 01:46:09 +0300

    debag nps.cpp

--- receptCode/receptCode/nps.cpp
+++ receptCode/receptCode/nps.cpp
@@ -30,7 +30,7 @@ void Nps::showVegStatus() const {
     else cout << "is not vegetarian" << endl;
 }
 
-void Nps::chooseRecepy(const Recepy& recepy) const {  //composition
+void Nps::chooseRecepy(const Recepy& recepy) const {
     cout << name << " decides to cook: " << endl;
     cout << "Recipe: " << recepy.getName() << ", Page: " << recepy.getPage() << endl;
     recepy.meatStatus();
