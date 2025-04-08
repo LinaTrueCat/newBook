fbcfceed7d00993959a76ab34383fc26090ab3c9
LinaTrueCat <146427251+LinaTrueCat@users.noreply.github.com>
2025-04-09 01:44:34 +0300

    add modificator final

--- receptCode/receptCode/mainDish.h
+++ receptCode/receptCode/mainDish.h
@@ -2,14 +2,21 @@
 #define MAINDISH_H
 
 #include "recepy.h"
+#include "cooking.h"
 
-class MainDish : public Recepy {
+class MainDish final : public Recepy, public cooking {     //
 private:
     int calories;
 
 public:
     MainDish(string n, int p, bool m, int cals);
     void showCalories() const;
+
+    void recepyStatus() const override;
+    void meatStatus() const override;
+    void showType() const override;
+
+    void cook() const override;
 };
 
-#endif // MAINDISH_H
+#endif 
