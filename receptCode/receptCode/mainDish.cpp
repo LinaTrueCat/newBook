2b00bc37ea87b69607b03e5eb18e3121d4e9811a
LinaTrueCat <146427251+LinaTrueCat@users.noreply.github.com>
2025-04-09 01:38:10 +0300

    add virtual destructor

--- receptCode/receptCode/mainDish.cpp
+++ receptCode/receptCode/mainDish.cpp
@@ -6,6 +6,24 @@ MainDish::MainDish(string n, int p, bool m, int cals)
     std::cout << "Main dish created: " << n << " with " << calories << " calories" << std::endl;
 }
 
+void MainDish::recepyStatus() const {    //
+    std::cout << "[MainDish] ";
+    Recepy::recepyStatus();
+}
+
+void MainDish::meatStatus() const {      //
+    std::cout << "[MainDish] ";
+    Recepy::meatStatus();
+}
+
 void MainDish::showCalories() const {
     std::cout << "Calories: " << calories << std::endl;
+}
+
+void MainDish::showType() const {
+    std::cout << "This is a MainDish type" << std::endl;   //
+}
+
+void MainDish::cook() const {
+    std::cout << "Cooking MainDish: " << getName() << std::endl;
 }
